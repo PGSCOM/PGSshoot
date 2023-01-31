74fba15e99651c9cc7655bc585ef06976e0b500e
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-01-31 19:56:05 +0100

    Initial commit

---
+++ Assets/FPS/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+public void Evento () {;
+Application.OpenURL("https://www.youtube.com/channel/UCh1v_AgYrXej4bxBEFdNutQ");
+}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PGSshoot
+PGSshoot es un juego FPS hecho con Unity  con la plantilla de FPS game. Es un proyecto pequeño, lo iré actualizando poco a poco.
